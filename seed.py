d5ba3aabe9e54d51d68a2d1dc65fd7d13eadbcbd
DESKTOP-KQA2RP4\ThinkPad <chinhpham1609@gmail.com>
2024-10-09 20:55:38 +0700

    seed update

--- seed.py
+++ seed.py
@@ -51,39 +51,56 @@ class Seed:
         return response
     def main(self):
         while True:
-            query_id = f"query_id=AAESTRtAAwAAABJNG0D0mg26&user=%7B%22id%22%3A7517981970%2C%22first_name%22%3A%22Dao%20%F0%9F%8D%85%20%F0%9F%90%88%E2%80%8D%E2%AC%9B%20%F0%9F%8C%B1SEED%22%2C%22last_name%22%3A%22Nguyen%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1728462449&hash=1c2ea74848e7957f58e56562f3e35e6eb2cd36bdfa3e3f5f28487af0330b1fab"
-            balance = self.balance(query_id).json()
-            common.log(f"{common.blue}Balance: {balance['data']}")
+            query_id = f"query_id=AAESTRtAAwAAABJNG0DbVK0F&user=%7B%22id%22%3A7517981970%2C%22first_name%22%3A%22Dao%20%F0%9F%8D%85%20%F0%9F%90%88%E2%80%8D%E2%AC%9B%20%F0%9F%8C%B1SEED%22%2C%22last_name%22%3A%22Nguyen%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1728481815&hash=ee4bb117e8d1e209a7089d7a4b2bf928ee34aea4c3c53073e8e8fa5b6650b6b1"
+            try:
+                balance = self.balance(query_id).json()
+                print(balance)
+                balance_data = balance["data"]
+                common.log(f"{common.blue}Balance: {balance_data}")
+            except Exception as e:
+                common.log(e)
 
             time.sleep(1)
             # Get worms
-            worms = self.get_worms(query_id).json()
-            next_worm = worms["data"]["next_worm"]
-            common.log(f"Next worm: {next_worm}")
+            try:
+                worms = self.get_worms(query_id).json()
+                next_worm = worms["data"]["next_worm"]
+                common.log(f"Next worm: {next_worm}")
+            except Exception as e:
+                common.log(e)
 
             time.sleep(1)
-            claim = self.claim(query_id).json()
-            if claim["code"] == "invalid-request":
-                common.log("Claim too early!")
-            else:
-                amount = claim["data"]["amount"]
-                common.log(f"Amount claim: {amount}")
+            try:
+                claim = self.claim(query_id).json()
+                if claim["code"] == "invalid-request":
+                    common.log("Claim too early!")
+                else:
+                    amount = claim["data"]["amount"]
+                    common.log(f"Amount claim: {amount}")
+            except Exception as e:
+                common.log(e)
 
             # check daily
-            time.sleep(1)
-            check_daily = self.login_bonus(query_id).json()
-            day = check_daily["data"][0]["no"]
-            common.log(f"Kiem tra ngay thu: {day}")
+            try:
+                time.sleep(1)
+                check_daily = self.login_bonus(query_id).json()
+                day = check_daily["data"][0]["no"]
+                common.log(f"Kiem tra ngay thu: {day}")
+            except Exception as e:
+                common.log(e)
 
-            time.sleep(1)
-            tasks = self.get_tasks(query_id).json()
-            task_list = tasks["data"]
+            try:
+                time.sleep(1)
+                tasks = self.get_tasks(query_id).json()
+                task_list = tasks["data"]
 
-            for task in task_list:
-                time.sleep(random.randint(5,20))
-                claim_task = self.get_task(query_id, task["id"])
-                task_name = task["name"]
-                common.log(f"{task_name} is completed!")
+                for task in task_list:
+                    time.sleep(random.randint(5,20))
+                    claim_task = self.get_task(query_id, task["id"])
+                    task_name = task["name"]
+                    common.log(f"{task_name} is completed!")
+            except Exception as e:
+                common.log(e)
 
             wait_time = random.randint(3600, 7200)
             common.log(f"Wait time: {wait_time} minutes")
